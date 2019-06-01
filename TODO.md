673c3a587cda61c38fc334ceb21305e844857993
Akilan1999 <Akilanselva@hotmail.com>
2019-06-01 20:15:42 +0400

    Protototype code ready

--- TODO.md
+++ TODO.md
@@ -1,1 +1,2 @@
 # TODO list:
+1. To Finish API 2 : Get Estimated time for Electricity Transfer
--- app.js
+++ app.js
@@ -14,18 +14,11 @@ var path = require('path');
 var app = express();
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'app/views'));
-app.set('view engine', 'ejs');
 
 //Configure body parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-//Initialize session
-app.use(session({
-    secret: 'I Love India...',
-    resave: true,
-    saveUninitialized: true
-}));
 
 //For routes
 require('./config/routes.js')(app);
